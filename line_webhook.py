410ac6ea20e0aeef8114de1636530ae97c82b7ab
takahisa endo <takahisaendo@userenoMacBook-puro-2.local>
2026-06-09 16:42:23 +0700

    Rename LINE env vars to avoid Railway build secret conflict

--- line_webhook.py
+++ line_webhook.py
@@ -19,8 +19,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_SIGNING_KEY")
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 
 app = FastAPI()
 
